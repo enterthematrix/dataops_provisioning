f2537145372e33c8a7e8fa1e9befcb59f2b6177f
sanju <sanjeev.it@gmail.com>
2024-09-10 18:06:05 +1200

    util to create stage lib list

---
+++ lib_parser.py
@@ -0,0 +1,19 @@
+import ast
+
+def strip_library_name(library):
+    # Remove the prefix and suffix
+    return library.replace("streamsets-datacollector-", "").replace("-lib:5.12.0", "")
+
+# Stage lib for SDC
+with open('config/sdc/sdc_stage_libs.txt', 'r') as file:
+    libraries = file.read()
+libraries = ast.literal_eval(libraries)
+
+stripped_libraries = [strip_library_name(lib) for lib in libraries]
+with open('config/sdc/sdc_stage_libs.conf', 'w') as file:
+    for library in libraries:
+        stripped_name = strip_library_name(library)
+        file.write(stripped_name + '\n')
+
+print("Stage lib list written to sdc_stage_libs.conf")
+
